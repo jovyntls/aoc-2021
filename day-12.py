ef8136337119c5037e43c8876e308ddf3f2980d0
Jovyn Tan <jovyn.tls@gmail.com>
2021-12-12 16:19:45 +0800

    solution for day 12

--- day-12.py
+++ day-12.py
@@ -7,14 +7,15 @@ class Node:
 with open('input.txt') as f:
     edges = [x for x in f.read().split('\n') if x != '']
 
-GRAPH_NODES = {}
+graph_nodes = {}
 for edge in edges:
     n1, n2 = edge.split('-')
-    if n1 not in GRAPH_NODES: GRAPH_NODES[n1] = Node(n1)
-    if n2 not in GRAPH_NODES: GRAPH_NODES[n2] = Node(n2)
-    n1, n2 = GRAPH_NODES[n1], GRAPH_NODES[n2]
+    if n1 not in graph_nodes: graph_nodes[n1] = Node(n1)
+    if n2 not in graph_nodes: graph_nodes[n2] = Node(n2)
+    n1, n2 = graph_nodes[n1], graph_nodes[n2]
     n1.neighbours.add(n2)
     n2.neighbours.add(n1)
+START_NODE = graph_nodes['start']
 
 
 def part_1():
@@ -24,7 +25,7 @@ def part_1():
         if current_node.is_small: visited = set.union(visited, {current_node})
         return sum([find_paths(visited, neighbour) for neighbour in current_node.neighbours])
 
-    return find_paths(set(), GRAPH_NODES['start'])
+    return find_paths(set(), START_NODE)
 
 def part_2():
     def find_paths(visited, current_node, visited_twice):
@@ -36,7 +37,7 @@ def part_2():
             else: visited = set.union(visited, {current_node})
         return sum([find_paths(visited, neighbour, visited_twice) for neighbour in current_node.neighbours])
 
-    return find_paths({GRAPH_NODES['start']}, GRAPH_NODES['start'], set())
+    return sum([find_paths({START_NODE}, node, {START_NODE}) for node in START_NODE.neighbours])
 
 
 print("PART 1: ", part_1())
